e0b699898ef3f1ea4029f94fa974e8e47a7118ac
Dendos <cotoha92@gmail.com>
2017-10-04 22:44:55 +0200

    create UserWithName model

---
+++ spec/support/models.rb
@@ -0,0 +1,51 @@
+load File.dirname(__FILE__) + '/schema.rb'
+
+class User < ActiveRecord::Base
+  extend JsonDefaults
+  
+  json_defaults(
+    field: "params", 
+    options: {
+      name: "Denis",
+      programmer: {
+        value: true,
+        boolean: true
+      },
+      age: {
+        value: 25,
+        integer: true
+      },
+      data: {
+        field1: 'field1',
+        field2: 'field2',
+      }
+
+    }
+  )
+
+end
+
+class UserWithName < ActiveRecord::Base
+  extend JsonDefaults
+  
+  json_defaults(
+    field: "params", 
+    options: {
+      name: "Denis",
+      programmer: {
+        value: true,
+        boolean: true
+      },
+      age: {
+        value: 25,
+        integer: true
+      },
+      data: {
+        field1: 'field1',
+        field2: 'field2',
+      }
+
+    }
+  )
+
+end
